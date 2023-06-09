6600e23a1e6c6324cf112065a9d7287b92895bea
nargis21 <nargisakther1933@gmail.com>
2023-06-09 22:56:44 +0600

    [Done] Create academic semester constant

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -3,31 +3,21 @@ import {
   AcademicSemesterModel,
   IAcademicSemester,
 } from './academicSemester.interface';
+import {
+  academicSemesterCodes,
+  academicSemesterMonths,
+  academicSemesterTitles,
+} from './academicSemester.constant';
 
 // 2. Create a Schema corresponding to the document interface.
 
-const month = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
-    title: { type: String, required: true, enum: ['Autumn', 'Summer', 'Fall'] },
+    title: { type: String, required: true, enum: academicSemesterTitles },
     year: { type: Number, required: true },
-    code: { type: String, required: true, enum: ['01', '02', '03'] },
-    startMonth: { type: String, required: true, enum: month },
-    endMonth: { type: String, required: true, enum: month },
+    code: { type: String, required: true, enum: academicSemesterCodes },
+    startMonth: { type: String, required: true, enum: academicSemesterMonths },
+    endMonth: { type: String, required: true, enum: academicSemesterMonths },
   },
   {
     timestamps: true,
